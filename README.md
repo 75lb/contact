643b247b6a13a2c4d520e9e91fba6cbdbc263b51
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-06-08 23:21:59 +0100

    decouple

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 Contact
 =======
+Say you want to chat in private with a friend over a direct, peer-to-peer connection with zero intrusion. Contact's job is to open a direct, secure connection over which you can chat. That's it. 
+
+Install
+-------
+```sh
+$ npm install -g contact
+```
+*Linux/Mac users may need to run the above with `sudo`*
+
+Usage
+-----
+**Listen**
+```sh
+$ connect -l
+```
+
+**Connect**
+```sh
+$ connect <ip>
+```
 
 [![githalytics.com alpha](https://cruel-carlota.pagodabox.com/edfb933b1e0a698ebac84ebc658732f3 "githalytics.com")](http://githalytics.com/75lb/contact)
--- contact.js
+++ contact.js
@@ -1,55 +1,104 @@
 #!/usr/bin/env node
+"use strict";
 var net = require("net"),
     readline = require("readline"),
+    EventEmitter = require("events").EventEmitter,
+    util = require("util"),
     rl = readline.createInterface({ input: process.stdin, output: process.stdout });
     
 var l = console.log,
     port = 6667;
 
-if (process.argv[2] === "-l"){
-    var server = net.createServer(function(c){
-        l("client connected");
-        c.on("end", function(){
-            l("client disconnected");
-        });
-        c.on("data", function(data){
-            rl.pause();
-            l("\n" + data.toString());
-            rl.prompt(true);
-            rl.resume();
-        });
-        rl.prompt();
-        rl.on("line", function(line){
-            c.write(line);
-            rl.prompt();
-        });
-        rl.on("close", function(){
-            process.exit(0);
-        });
-    
-    }).listen(port);
-    
-} else {
-    var client = net.connect({ port: port, host: process.argv[2] }, function(){
-        l("client connected");
-        rl.prompt();
-        rl.on("line", function(line){
-            client.write(line);
-            rl.prompt();
-        });
-        rl.on("close", function(){
-            process.exit(0);
-        });
-    });
+/**
+State Machine:
+disconnected -> connected -> disconnected
+connected -> typing -> connected
+*/
+function Session(speakingTo, socket){
+    var self = this;
+    l("New Session with: " + speakingTo.ip);
+    this.state = "connected";
+    this.speakingTo = speakingTo;
+    this.socket = socket;
+    this.disconnect = function(){ 
+        l("session disconnected");
+    };
+    this.send = function(msg){
+        this.socket.write(msg);
+    };
+    this.incomingMsg = function(msg){
+        this.emit("incomingMsg", msg);
+    };
+}
+util.inherits(Session, EventEmitter);
 
-    client.on("data", function(data){
+function User(ip, name){
+    this.ip = ip;
+    this.name = name;
+}
+
+function TerminalView(){
+    var self = this;
+    rl.on("line", function(line){
+        self.emit("input", line);
+        rl.prompt();
+    });
+    rl.on("close", function(){
+        process.exit(0);
+    });
+    this.prompt = function(){
+        rl.prompt();
+    };
+    this.showMessage = function(msg){
         rl.pause();
-        l("\n" + data.toString());
+        l("\n" + msg.toString());
         rl.prompt(true);
         rl.resume();
-    });
-    client.on("end", function(){
-        l("client disconnected");
+    };
+    
+}
+util.inherits(TerminalView, EventEmitter);
+
+function Manager(transport, view){
+    transport[process.argv[2] === "-l" ? "listen" : "connect"](function(session){
+        view.prompt();
+        view.on("input", function(msg){
+            session.send(msg);
+        });
+        session.on("incomingMsg", function(msg){
+            view.showMessage(msg);
+        });
     });
 }
 
+function NodeTransport(){
+    this.listen = function(callback){
+        l("listening on port " + port);
+        var server = net.createServer(function(socket){
+            var session = new Session(
+                new User(socket.remoteAddress, "<unknown>"),
+                socket
+            );
+            socket.on("end", session.disconnect);
+            socket.on("data", function(data){
+                session.incomingMsg(data);
+            });
+            callback(session);
+        }).listen(port);
+    };
+    this.connect = function(callback){
+        var socket = net.connect({ port: port, host: process.argv[2] }, function(){
+            var session = new Session(
+                new User(socket.remoteAddress, "<unknown>"),
+                socket
+            );
+            socket.on("end", session.disconnect);
+            socket.on("data", function(data){
+                session.incomingMsg(data);
+            });
+            callback(session);
+        });
+    }
+}
+
+Manager(new NodeTransport(), new TerminalView());
