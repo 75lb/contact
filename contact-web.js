6bc2c86683fd4671aafdadb8ea4631c384c3e625
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-03-07 22:39:42 +0100

    wip

--- contact-web.js
+++ contact-web.js
@@ -1,80 +1,14 @@
+"use strict";
 var TransportWeb = require("./lib/TransportWeb"),
     ViewWeb = require("./lib/ViewWeb");
 
 var transport = new TransportWeb(),
-    view = new ViewWeb(),
     options = { 
-        host: "localhost",
-        port: 4444
+        host: "serene-stream-2466.herokuapp.com"
     };
 
 transport.connect(options, function(session){
     console.log("BOOM");
-    session.on("incomingMsg", function(msg){
-        view.showMessage(msg);
-    });
-    view.on("input", function(msg){
-        session.send(msg);
-    });
+    session.setView(new ViewWeb());
+    session.me = "Dave";
 });
-
-
-// function l(msg){ console.log(msg); }
-// 
-// function Connection(options){
-//     this.onOpen = null;
-//     this.onClose = null;
-//     this.onData = null;
-//     this.onError = null;
-//     this.send = null;
-// }
-// 
-// 
-// 
-// /**
-// state machine:
-// disconnected -> connected -> typing
-// */
-// function Session(options){
-//     var self = this;
-//     l("New Session with: " + options.with);
-//     this.state = "disconnected";
-//     this.with = options.with;
-//     this.connection = null;
-//     this.onIncoming = null;
-// 
-//     this.disconnect = function(){
-//         l("session disconnected");
-//         self.state = "disconnected";
-//     };
-//     this.send = function(msg){
-//         this.connection.send(msg);
-//     };
-// 
-//     options.transport.getConnection(this.with, function(connection){
-//         self.state = "connected";
-//         self.connection = connection;
-//         connection.onData = function(msg){
-//             self.onIncoming(msg);
-//         }
-//         options.onConnected(connection);
-//     });
-// }
-// 
-// var session = new Session({
-//     with: "ws://localhost:4444",
-//     transport: new TransportWeb(),
-//     onConnected: function(connection){
-//         l("connected", connection);
-//     }
-// });
-// 
-// 
-// var view = new WebView();
-// view.onInput = function(input){
-//     session.send(input);
-// };
-// 
-// session.onIncoming = function(msg){
-//     view.write(msg);
-// };
--- contact.js
+++ contact.js
@@ -22,3 +22,24 @@ transport.connect({ host: "serene-stream-2466.herokuapp.com" }, function(session
     session.setView(new ViewTerminal());
     session.me = argv.user;
 });
+
+// chat history, presence (arrived, left), /me, /who (is online), formatting, ssl, /info (about connection, connected users and their IPS), /ban, /kick, connection keep-alive, 
+// web client host in query string, notifications
+
+// var transportWeb = new TransportWeb();
+// transportWeb.connect({ host: "blah"}, function(session){
+//     session            // { type: "message", data: { user: "A", msg: "you there?" }}
+//         .pipe(history) // { type: "message", data: { user: "A", msg: "you there?" }}
+//         .pipe(view)    // { type: "message", data: { user: "B", msg: "Yes i am" }}
+//         .pipe(slashMe) // { type: "message", data: { user: "B", msg: "Yes i am" }}
+//         .pipe(history) // { type: "message", data: { user: "B", msg: "Yes i am" }}
+//         .session       // { type: "message", data: { user: "B", msg: "Yes i am" }}
+// 
+//     session                 // { type: "message", data: { user: "A", msg: "you there?" }}
+//         .pipe(viewFile)     // { type: "message", data: { user: "A", msg: "you there?" }}
+//         .pipe(viewTerminal) // { type: "message", data: { user: "B", msg: "/me nods" }}
+//         .pipe(slashMe)      // { type: "action",  data: { user: "B", action: "nods"  }}
+//         .pipe(viewFile)     // { type: "action",  data: { user: "B", action: "nods"  }}
+//         .session            // { type: "message", data: { user: "B", msg: "Yes i am" }}
+// 
+// })
